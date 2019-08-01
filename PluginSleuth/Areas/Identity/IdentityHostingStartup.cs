907c7c01943379191e9f7d70e6dec6093295536a
anneraev <47757104+anneraev@users.noreply.github.com>
2019-08-01 13:18:41 -0500

    refactor code back to MVC

---
+++ PluginSleuth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PluginSleuth.Data;
+
+[assembly: HostingStartup(typeof(PluginSleuth.Areas.Identity.IdentityHostingStartup))]
+namespace PluginSleuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
