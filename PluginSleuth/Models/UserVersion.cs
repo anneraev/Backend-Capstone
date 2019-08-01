fd112fbd6cad7e1112d2bb6c65103e7be0656ef8
anneraev <47757104+anneraev@users.noreply.github.com>
2019-08-01 15:05:50 -0500

    seeded database again

---
+++ PluginSleuth/Models/UserVersion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluginSleuth.Models
+
+{
+    public class UserVersion
+    {
+        [Key]
+        [Required]
+        public int UserVersionId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public ApplicationUser User { get; set; }
+        [Required]
+        [ForeignKey("Version")]
+        public int VersionId { get; set; }
+        public Version Version { get; set; }
+    }
+}
---
+++ PluginSleuth/Models/Version.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluginSleuth.Models
+
+{
+    public class Version
+    {
+        [Key]
+        public int VersionId { get; set; }
+        [Required]
+        [StringLength(255, ErrorMessage = "Please shorten the name to 50 characters")]
+        public string Name { get; set; }
+        public string ReadMe { get; set; }
+        [Required]
+        [ForeignKey("Plugin")]
+        public int PluginId { get; set; }
+        public Plugin Plugin { get; set; }
+        public string DownloadLink { get; set; }
+        public List<UserVersion> UserVersions { get; set; }
+        [Required]
+        public int Iteration { get; set; }
+    }
+}
