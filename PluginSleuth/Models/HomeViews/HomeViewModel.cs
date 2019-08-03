c95e5245e2d9111c471d880348c803dc25cf68f4
anneraev <47757104+anneraev@users.noreply.github.com>
2019-08-03 15:20:27 -0500

    created front page view with engines and random featured plugin

---
+++ PluginSleuth/Models/HomeViews/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluginSleuth.Models.HomeViews
+{
+    public class HomeViewModel
+    {
+        public Plugin Plugin { get; set; }
+        public List<Engine> Engines { get; set; }
+    }
+}
